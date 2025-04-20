63f774869171b76e185a95bc5a3330146aa57641
carlos.perez <carlospverano@gmail.com>
2025-04-19 21:15:29 -0500

    feat: add date-fns-tz dependency and enhance assignment modal with improved styling and functionality

--- src/app/componentes/asignacion-horario/asignacion-horario.component.ts
+++ src/app/componentes/asignacion-horario/asignacion-horario.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AdministradorService } from '../../servicios/administrador.service';
 import { ItemEmpleadoDTO } from '../../dto/item-empleado-dto';
@@ -14,6 +14,8 @@ import { ItemEmpleadoDTO } from '../../dto/item-empleado-dto';
 export class AsignacionHorarioComponent {
   asignarHorarioForm!: FormGroup;
   empleados: ItemEmpleadoDTO[] = [];
+  @Output() closeModalEvent = new EventEmitter<void>();
+  @Input() fechaSeleccionada!: String ;
 
   constructor(private formBuilder: FormBuilder, private adminService: AdministradorService) {
     this.crearFormulario();
@@ -23,21 +25,18 @@ export class AsignacionHorarioComponent {
 
   private crearFormulario() {
     this.asignarHorarioForm = this.formBuilder.group({
-          cedula: ['', [Validators.required]],
-          nombre: ['', [Validators.required]],
-          apellido: ['', [Validators.required]],
-          email: ['', [Validators.required, Validators.email]],
-          telefono: ['', [Validators.required]],
-          departamento: ['', [Validators.required]],
-          cargo: ['', [Validators.required]],
-          fechaContratacion: ['', [Validators.required]],
+          horaInicio: ['', [Validators.required]],
+          horaFin: ['', [Validators.required]],
+          empleado: ['', [Validators.required]],
           
         });
 
   } 
 
   public cleanFields() {
-
+    this.asignarHorarioForm.reset();
+    this.closeModalEvent.emit();
+    
   }
 
   public addAsignacion() {
--- src/app/componentes/horario/horario.component.ts
+++ src/app/componentes/horario/horario.component.ts
@@ -6,6 +6,8 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { HeaderComponent } from "../header/header.component";
 import { AsignacionHorarioComponent } from "../asignacion-horario/asignacion-horario.component";
 import { CommonModule } from '@angular/common';
+import { enUS, es } from 'date-fns/locale';
+import { format } from 'date-fns'
 
 @Component({
   selector: 'app-horario',
@@ -21,24 +23,59 @@ export class HorarioComponent {
   horasAsignadas: number = 156;
   empleadosAsignados: number = 12;
   verAsignacion: boolean = false;
+  fechaSeleccionada!: string;
 
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin, interactionPlugin],
     dateClick: (arg) => this.handleDateClick(arg),
+    eventClick: this.handleEventClick.bind(this),
     initialView: 'dayGridMonth',
-    weekends: false,
+    weekends: true,
     dayCellDidMount: this.estilizarCeldaDia.bind(this),
     events: [
-      { title: 'Meeting', start: new Date() }
+      { title: 'Reunión equipo A', start: this.generarFecha('2025-04-08T10:00:00') },
+      { title: 'Reunión equipo A', start: this.generarFecha('2025-04-08T10:00:00') },
+      { title: 'Capacitación interna', start: this.generarFecha('2025-04-12T14:00:00') },
+      { title: 'Entrega reporte', start: this.generarFecha('2025-04-15T09:00:00') },
+      { title: 'Llamada con cliente', start: this.generarFecha('2025-04-20T11:00:00') },
+      { title: 'Cierre mensual', start: this.generarFecha('2025-04-25T16:00:00') },
+      { title: 'Taller técnico', start: this.generarFecha('2025-04-28T13:00:00') }
     ]
   }
 
   handleDateClick(arg: any) {
-    // alert('date click! ' + arg.dateStr);
-    this.verAsignacion = true;
+    const clickedDate = new Date(arg.dateStr);
+    const today = new Date();
+
+    clickedDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+
+    if (clickedDate >= today) {
+      this.verAsignacion = true;
+      const fechaLocal = new Date(clickedDate.getFullYear(), clickedDate.getMonth(), clickedDate.getDate()+1)
+      this.fechaSeleccionada = format(fechaLocal, "EEEE, d 'de' MMMM", { locale: es });
+    }
   }
 
   estilizarCeldaDia(info: any) {
-    info.el.classList.add('hover-cell');
+    const cellDate = new Date(info.date);
+    const today = new Date();
+  
+    cellDate.setHours(0, 0, 0, 0);
+    today.setHours(0, 0, 0, 0);
+  
+    if (cellDate < today) {
+      info.el.classList.add('past-cell');
+    } else {
+      info.el.classList.add('hover-cell');
+    }
+  }
+
+  generarFecha(fechaISO: string): Date {
+    return new Date(fechaISO);
+  }
+
+  handleEventClick(info: any) {
+    alert('Evento: ' + info.event.title + '\nFecha: ' + info.event.start?.toISOString());
   }
 }
